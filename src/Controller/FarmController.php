3fd4ae5a1146bfcefa9edcd803adf3c1d355642a
djegbakokora <limbo430@gmail.com>
2022-07-23 22:26:04 +0200

    feature:update farm info

---
+++ src/Controller/FarmController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Dto\ForgottenPasswordInput;
+use App\Entity\Customer;
+use App\Entity\ForgottenPassword;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Producer;
+use App\Form\ForgottenPasswordType;
+use App\Form\RegistrationForm;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\ResetPasswordType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use App\Form\FarmType;
+
+/**
+ * @Route("/farm")
+*/
+class FarmController extends AbstractController
+{
+    /**
+     * @Route("/update", name="farm_update")
+    */
+    public function update(Request $request)
+    {
+        $form = $this->createForm(FarmType::class, $this->getUser()->getFarm())->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'Les informations de votre exploitation ont ete modifie avec success');
+
+            return $this->redirectToRoute('farm_update');
+        }
+        return $this->render('ui/farm/update.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/FarmType.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Farm;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class FarmType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("name", TextType::class);
+        $builder->add("description", TextareaType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault("data_class", Farm::class);
+    }
+}
